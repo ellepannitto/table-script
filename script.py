ca536f25af6afbd2cb4ba87b9351bde51b56d120
Ludovica Pannitto <ellepannitto@gmail.com>
2025-05-21 15:56:48 +0200

    symlink creation

--- script.py
+++ script.py
@@ -2,16 +2,33 @@ import pathlib
 import conllu
 import sys
 
+if len(sys.argv) < 3:
+	print("Please run `python3 script.py [SOURCE_DIR] [DEST_DIR]`")
+	sys.exit(1)
+
+source_path = sys.argv[1]
+destination_path = sys.argv[2]
+
+spoken_only = ["UD_Northwest_Gbaya-Autogramm"]
 
 conditions = {"UD_English-GENTLE": (lambda meta: "meta::speakerCount" in meta,
 									lambda meta: int(meta["meta::speakerCount"])>0 )
 			}
 
+for treebank in spoken_only:
+	print(f"Reading treebank {treebank}...")
+	source_dir = f"{source_path}/{treebank}/"
+	destination_dir = f"{destination_path}/{treebank}"
+	try:
+		pathlib.Path(destination_dir).symlink_to(source_dir)
+	except FileExistsError as e:
+		print("Symlink already in place")
+
 
 for treebank in conditions:
 	print(f"Reading treebank {treebank}...")
-	source_path = f"{sys.argv[1]}/{treebank}/"
-	destination_path = f"{sys.argv[2]}/{treebank}"
+	source_dir = f"{source_path}/{treebank}/"
+	destination_dir = f"{destination_path}/{treebank}"
 	pathlib.Path(destination_path).mkdir(parents=True, exist_ok=True)
 
 
@@ -20,7 +37,6 @@ for treebank in conditions:
 		print(f"Reading file {filename}...")
 
 		file_stem = filename.stem
-
 		with open(filename) as fin, \
 			open(pathlib.Path(destination_path).joinpath(f"{file_stem}.selected.conllu"), "w") as fout:
 
@@ -41,3 +57,4 @@ for treebank in conditions:
 				else:
 					if condition:
 						print(sentence.serialize().strip(), file=fout)
+				print("", file=fout)
