cda55c6b3b70b20889667ed2be52c60ad1473125
Ludovica Pannitto <ellepannitto@gmail.com>
2025-05-21 15:31:38 +0200

    first output

--- script.py
+++ script.py
@@ -1,10 +1,44 @@
-from conllu import parse
+import pathlib
+import conllu
 
 
-with open("source/UD_English-GENTLE/en_gentle-ud-test.conllu") as fin:
-	data = fin.read()
-	sentences = parse(data)
+conditions = {"UD_English-GENTLE": (lambda meta: "meta::speakerCount" in meta,
+									lambda meta: int(meta["meta::speakerCount"])>0 )
+			}
 
-	for sentence in sentences:
-		print(sentence.TokenList)
-		input()
+
+for treebank in conditions:
+	print(f"Reading treebank {treebank}...")
+	source_path = f"source/{treebank}/"
+	destination_path = f"destination/{treebank}"
+	pathlib.Path(destination_path).mkdir(parents=True, exist_ok=True)
+
+
+	for filename in pathlib.Path(source_path).glob("*.conllu"):
+
+		print(f"Reading file {filename}...")
+
+		file_stem = filename.stem
+
+		with open(filename) as fin, open(pathlib.Path(destination_path).joinpath(f"{file_stem}.selected.conllu"), "w") as fout:
+
+			fun1, fun2 = conditions[treebank]
+			sentences = conllu.parse_incr(fin)
+
+			condition = False
+
+			for sentence in sentences:
+				metadata = sentence.metadata
+
+				if fun1(metadata):
+					if fun2(metadata):
+						condition = True
+						print(sentence.serialize().strip(), file=fout)
+					else:
+						condition = False
+				else:
+					if condition:
+						print(sentence.serialize().strip(), file=fout)
+
+		if condition:
+			print(sentence.serialize().strip(), file=fout)
